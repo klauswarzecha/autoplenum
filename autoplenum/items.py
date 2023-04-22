4d8916b0d4545ec87ce565696cd9d41bf1d4e4dc
Klaus-Dieter Warzecha <klauswarzecha@gmail.com>
2023-04-22 14:43:06 +0200

    Add fields to item class

--- autoplenum/items.py
+++ autoplenum/items.py
@@ -1,12 +1,21 @@
 # Define here the models for your scraped items
-#
-# See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
 
 
 class AutoplenumItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    companyname = Field()
+    street = Field()
+    postalcode = Field()
+    location = Field()
+
+    phone = Field()
+    fax = Field()
+    website = Field()
+    
+    services = Field()
+
+    review_count = Field()
+    rating_count = Field()
+    rating_average = Field()
