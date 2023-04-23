fbc71b4945b21a742ebd4f878dff4a80b9803d4a
Klaus-Dieter Warzecha <klauswarzecha@gmail.com>
2023-04-23 07:18:29 +0200

    Correct fieldname for locality

--- autoplenum/items.py
+++ autoplenum/items.py
@@ -2,13 +2,13 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-
+from scrapy.item import Field
 
 class AutoplenumItem(scrapy.Item):
     companyname = Field()
     street = Field()
     postalcode = Field()
-    location = Field()
+    locality = Field()
 
     phone = Field()
     fax = Field()
